3a3ef283aafc8f6bfcbf87e0e8d42808c3e9e35c
Ankan Pal <ankan1811@gmail.com>
2021-04-14 18:59:47 +0530

    Update Join.js

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -12,12 +12,16 @@ export default function SignIn() {
       <div className="joinInnerContainer">
         <h1 className="heading">Join</h1>
         <div>
-          <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
+          <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} /> //Since we are using state it won't be this.setName or something
         </div>
         <div>
-          <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
+          <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />//mt-20 means margin top of 20
         </div>
+//If no name or room is given it will lead to event.prevent default and it won't go to /chat
+//If they are present we will do nothing since we want data to be just transferred to our chat
+//<Link to={‘url ? pass the parameters separated by and}>
         <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+
           <button className={'button mt-20'} type="submit">Sign In</button>
         </Link>
       </div>
