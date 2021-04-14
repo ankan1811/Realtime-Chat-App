e25ed8e8859c9536929f9a5d9362a6beb5ae5731
Ankan Pal <ankan1811@gmail.com>
2021-04-14 20:22:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,10 +19,11 @@ io.on('connect', (socket) => {
   //this socket will be connected as a client side socket
   //io.on is a method and it will run when we have a client connection on our io instance
   //We will use this method connect to register clients joining into our chat app
-  socket.on('join', ({ name, room }, callback) => {
+  
+  socket.on('join', ({ name, room }, callback) => { //we mentioned this in client/chat.js. we get these data from the frontend.
     const { error, user } = addUser({ id: socket.id, name, room });
 
-    if (error) return callback(error);
+    if (error) return callback(error); //we have access to this callback as a toward parameter to emit fi=unction
 
     socket.join(user.room);
 
@@ -31,7 +32,7 @@ io.on('connect', (socket) => {
 
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
-    callback();
+    callback();//wec can trigger some response immediately after socket.on is being emmitted and we will do error handling
   });
 
   socket.on('sendMessage', (message, callback) => {
