6b6fafadc8664f298f03ec3920808d6355b23c29
Ankan Pal <ankan1811@gmail.com>
2021-04-15 20:36:30 +0530

    Update Messages.js

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,4 +1,6 @@
+//In Messages.js we loop through all the messages but we do not have access to it so we pass the messages as props in chat.js
 import React from 'react';
+
 
 import ScrollToBottom from 'react-scroll-to-bottom';
 
@@ -6,7 +8,7 @@ import Message from './Message/Message';
 
 import './Messages.css';
 
-const Messages = ({ messages, name }) => (
+const Messages = ({ messages, name }) => ( //for each message we will generate a key which is index i
   <ScrollToBottom className="messages">
     {messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
   </ScrollToBottom>
