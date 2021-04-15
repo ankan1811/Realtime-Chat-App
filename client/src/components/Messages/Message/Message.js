b54d72b614d930c25474942b253257fc5fa252ea
Ankan Pal <ankan1811@gmail.com>
2021-04-15 20:51:16 +0530

    Update Message.js

--- client/src/components/Messages/Message/Message.js
+++ client/src/components/Messages/Message/Message.js
@@ -7,7 +7,8 @@ import ReactEmoji from 'react-emoji';
 const Message = ({ message: { text, user }, name }) => {
   let isSentByCurrentUser = false;
 
-  const trimmedName = name.trim().toLowerCase();
+  const trimmedName = name.trim().toLowerCase(); //we trim the name similarly like backend
+  //So Ankan Pal will be ankanpal
 
   if(user === trimmedName) {
     isSentByCurrentUser = true;
@@ -15,7 +16,7 @@ const Message = ({ message: { text, user }, name }) => {
 
   return (
     isSentByCurrentUser
-      ? (
+      ? ( //if the message is sent by the current user use blue background
         <div className="messageContainer justifyEnd">
           <p className="sentText pr-10">{trimmedName}</p>
           <div className="messageBox backgroundBlue">
@@ -23,7 +24,7 @@ const Message = ({ message: { text, user }, name }) => {
           </div>
         </div>
         )
-        : (
+        : (//if the message is not sent by the current user use light background
           <div className="messageContainer justifyStart">
             <div className="messageBox backgroundLight">
               <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
